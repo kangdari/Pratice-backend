86e468bb9d347993f3bc1c60015be31124e66347
ksh44820@likelion.org <ksh44820@likelion.org>
2020-01-15 20:58:03 +0900

    MongoDB 설치 및 서버 준비 && mongoose 설치 및 적용

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
 // api 라우터 불러옴
 const api = require('./api')
+
+// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
+const { PORT, MONGO_URL } = process.env;
+
+mongoose
+    .connect(MONGO_URL, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true})
+    .then(()=>{
+        console.log('Connected to MongoDB');
+    })
+    .catch(e => {
+        console.error(e);
+});
+    
 
 const app = new Koa();
 const router = new Router();
@@ -16,6 +32,7 @@ app.use(bodyParser());
 // app 인스턴스에 라우저 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-    console.log('Listening to port 4000');
+const port = PORT || 4000;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
 })
